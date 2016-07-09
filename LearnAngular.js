801d295d97bec0211d0ee048f83d1de0347aee64
migg81 <samiran.majumder@gmail.com>
2016-07-10 00:06:28 +0530

    Test

--- LearnAngular.js
+++ LearnAngular.js
@@ -1,21 +1,3 @@
-/*(function () {
-    var app = angular.module("githubmodule", []);
-    var MainController = function ($scope,$http) {
-
-        $scope.message = "Hello Angular";
-    };
-
-   // $http.get("https://api.github.com/users/migg81").then(onUserComplete, onError);
-
-    $http.get("https://api.github.com/users/migg81").then(function(response) {
-        $scope.myWelcome = response.data;
-    });
-    app.controller('MainController',MainController);
-
-} ());*/
-
-
-
 (function () {
 
     var app = angular.module("githubmodule", []);
@@ -23,16 +5,22 @@
 
         $scope.message = "Hello Angular";
 
-
         var onUserComplete = function (response) {
             $scope.user = response.data;
+             $http.get($scope.user.repos_url).then(fetchRepoInfo, onError);
         };
 
         var onError = function (reason) {
             $scope.error = "Somthing went wron try after some time.";
         };
 
-        $http.get("https://api.github.com/users/migg81").then(onUserComplete, onError);
+        $scope.search=function fetchUserInfo(params) {
+            $http.get("https://api.github.com/users/"+ username).then(onUserComplete, onError);
+        }
+
+        var fetchRepoInfo=function(responce) {
+            $scope.repos=responce.data;
+        }
     };
 
     app.controller('MainController', ["$scope", "$http", MainController]);
