bfa41ec86f3519a22028698ed45b71f8f25764f7
migg81 <samiran.majumder@gmail.com>
2016-07-13 23:10:39 +0530

    New changes for node js and vs code

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+//app.use(express.static(path.join(__dirname, '../', 'LearnAngularModule')))
+app.use(express.static(__dirname + '/LearnAngularModule'));
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
+
+console.log("dir path : "+__dirname + '/LearnAngularModule');
